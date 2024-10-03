9519f3580cf8530d2b3b26fe232b71fb2fdcea36
marcussostu <marcus.so.stu@cedarpark.org>
2024-10-03 10:54:52 -0700

    print basic board

---
+++ tic-tac-toe.py
@@ -0,0 +1,20 @@
+board = [['.', '.', '.'],
+         ['.', '.', '.'],
+         ['.', '.', '.']]
+
+def print_board():
+    print(board[0])
+    print(board[1])
+    print(board[2])
+
+def play():
+    pass
+
+# MAIN PROGRAM ----------------------
+# peinr_instructions()
+
+print_board()
+# if no winner then repeat
+play()
+print_board()
+# check_winner()
